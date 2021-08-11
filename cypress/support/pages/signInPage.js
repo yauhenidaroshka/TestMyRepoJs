cccdc7d73fceeaf7731e41207897a47f4fc8de45
Yauheni Daroshka <yauhenidaroshka@Yauhenis-MacBook-Pro.local>
2021-08-11 15:07:29 +0300

    added sign in test

--- cypress/support/pages/signInPage.js
+++ cypress/support/pages/signInPage.js
@@ -5,14 +5,30 @@ export class SignInPage {
 
     pageHeader = '#greeting-msg'
     signInInputField = '//*[@id="userid"]'
+    continueButton = '#signin-continue-btn'
+    passwordField = '#pass'
+    signInButton = '#sgnBt'
 
     getPageHeader(){
         return cy.get(this.pageHeader)
 
     }
 
-    inputEmail(email){
-        cy.xpath(this.signInInputField).type(email)
+    inputEmail(email, string){
+        cy.xpath(this.signInInputField).type(email, {force: true})
+    }
+
+    clickContinueButton(){
+        cy.get(this.continueButton).click()
+
+    }
+
+    inputPassword(password, string){
+        cy.get(this.passwordField).click()
+        cy.get(this.passwordField).type(password, {force: true})
+    }
+    clickSingInButton(){
+        cy.get(this.signInButton).click()
     }
 
 }
